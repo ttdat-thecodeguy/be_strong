8c7686309b9f2bcbd3becf30b5600094ea6351e8
ttdat.99 <55250111+ttdat-thecodeguy@users.noreply.github.com>
2025-09-12 21:24:33 +0700

    initilize application

---
+++ src/main/java/com/datlab/beStrong/BeStrongApplication.java
@@ -0,0 +1,13 @@
+package com.datlab.beStrong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeStrongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeStrongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datlab/beStrong/BeStrongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datlab.beStrong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeStrongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
